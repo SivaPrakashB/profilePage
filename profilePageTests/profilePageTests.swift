476a25480e475ce4ab13f9b2be4b669ad9dd2381
Narayan <narayan@narayans-mac.local>
2018-05-14 17:52:38 +0530

    Initial Commit

---
+++ profilePageTests/profilePageTests.swift
@@ -0,0 +1,36 @@
+//
+//  profilePageTests.swift
+//  profilePageTests
+//
+//  Created by Narayan on 5/14/18.
+//  Copyright © 2018 senovTech. All rights reserved.
+//
+
+import XCTest
+@testable import profilePage
+
+class profilePageTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
